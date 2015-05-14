75001963a48c994578b0e2a48019cbb1dc80e3e6
Gary Haussmann <gjhaussmann@gmail.com>
2015-05-14 14:31:06 -0600

    Add pixi-dev to deps for clojurescript

--- src/deps.cljs
+++ src/deps.cljs
@@ -2,5 +2,5 @@
  [{:file "react_pixi/react-pixi.js"
    :file-min "react_pixi/react-pixi.min.js"
    :provides ["cljsjs.react"]}]
- :externs ["react_pixi/react-pixi.js"]
+ :externs ["react_pixi/react-pixi.js" "react_pixi/pixi.dev.js"]
  }
