8c185e5070d41d94a3a23ab7d6af005ec39dbf12
Gary Haussmann <gjhaussmann@gmail.com>
2017-05-23 11:27:02 -0600

    Update parameters used in patched receiveComponent

--- src/ReactPIXIMonkeyPatch.js
+++ src/ReactPIXIMonkeyPatch.js
@@ -60,23 +60,23 @@ let old_updateRenderedComponent = ReactCompositeComponent._updateRenderedCompone
 
 let ReactPIXI_updateRenderedComponent = function(transaction, context) {
   var prevComponentInstance = this._renderedComponent;
-  
+
   // Find the first actual rendered (non-Composite) component.
   // If that component is a PIXI node we use the special code here.
   // If not, we call back to the original version of updateComponent
   // which should handle all non-PIXI nodes.
-  
+
   let prevDisplayObject = findDisplayObjectChild(prevComponentInstance);
   if (!prevDisplayObject) {
     // not a PIXI node, use the original DOM-style version
     old_updateRenderedComponent.call(this,transaction, context);
     return;
   }
-  
+
   // This is a PIXI node, do a special PIXI version of updateComponent
   let prevRenderedElement = prevComponentInstance._currentElement;
   let nextRenderedElement = this._renderValidatedComponent();
-  
+
   if (shouldUpdateReactComponent(prevRenderedElement, nextRenderedElement)) {
     ReactReconciler.receiveComponent(
       prevComponentInstance,
@@ -89,30 +89,32 @@ let ReactPIXI_updateRenderedComponent = function(transaction, context) {
     // So we nuke the current instantiated component and put a new component in
     // the same place based on the new props.
     let thisID = this._rootNodeID;
-    
+
     let displayObjectParent = prevDisplayObject.parent;
-    
+
     // unmounting doesn't disconnect the child from the parent node,
     // but later on we'll simply overwrite the proper element in the 'children' data member
     let displayObjectIndex = displayObjectParent.children.indexOf(prevDisplayObject);
     ReactReconciler.unmountComponent(prevComponentInstance);
     displayObjectParent.removeChild(prevDisplayObject);
-    
+
     // create the new object and stuff it into the place vacated by the old object
     this._renderedComponent = this._instantiateReactComponent(
       nextRenderedElement,
       this._currentElement.type);
     let nextDisplayObject = ReactReconciler.mountComponent(
       this._renderedComponent,
-      thisID,
       transaction,
-      this._processChildContext(context)
+      this._hostParent,
+      this._hostContainerInfo,
+      this._processChildContext(context),
+      this._debugID
     );
     this._renderedComponent._displayObject = nextDisplayObject;
-    
+
     // fixup _mountImage as well
     this._mountImage = nextDisplayObject;
-    
+
     // overwrite the reference to the old child
     displayObjectParent.addChildAt(nextDisplayObject, displayObjectIndex);
   }
